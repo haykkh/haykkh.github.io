b916fb898e8fe22c548e213cec0e42ea9d1b0f60
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2018-09-13 13:42:21 +0400

    fontfamily in html header

--- src/js/script.js
+++ src/js/script.js
@@ -45,13 +45,3 @@ function autoDayNight() {
 }
 
 window.addEventListener("load", autoDayNight());
-
-var cb = function() {
-    var l = document.createElement('link'); l.rel = 'stylesheet';
-    l.href = '/css/style.css';
-    var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-  };
-  var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-      webkitRequestAnimationFrame || msRequestAnimationFrame;
-  if (raf) raf(cb);
-  else window.addEventListener('load', cb);
